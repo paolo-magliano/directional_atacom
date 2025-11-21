832b0a9ad69ec21e81586efc62640edd1921f8ba
Paolo Magliano <paolo.magliano@outlook.it>
2025-11-21 15:50:35 +0100

    Fix env import issue

--- cremini_rl/envs/__init__.py
+++ cremini_rl/envs/__init__.py
@@ -1,7 +1,8 @@
 from .ballnd_env import BallND
-from .air_hockey_env import AirHockeyEnv
-from .planar_air_hockey_env import PlanarAirhockeyEnv
+from .air_hockey_env import AirHockeyVel, AirHockeyAcc 
+from .planar_air_hockey_env import PlanarAirHockeyVel, PlanarAirHockeyAcc
 from .cartpole_goal_env import SafeCartPoleEnv
 from .tiago_navigation_env import TiagoNavigationEnv
 from .moving_obs import MovingObsEnv
 from .goal_navigation_env import GoalNavigationEnv
+from .quadrotor_env import QuadrotorEnv
