4470e43df68e5b5bbafa37c46f612eae2cae8a85
Paolo Magliano <paolo.magliano@outlook.it>
2025-11-21 15:43:57 +0100

    Redesign constraints structure

--- cremini_rl/dynamics/__init__.py
+++ cremini_rl/dynamics/__init__.py
@@ -3,4 +3,4 @@ from .cartpole_dynamics import CartPoleControlSystem
 from .moving_obs_dynamics import MovingObsDynamics
 from .goal_navigation_dynamics import GoalNavigationControlSystem
 from .tiago_navigation_dynamics import TiagoNavigationDynamics
-
+from .quadrotor_dynamics import QuadrotorControlSystem
--- cremini_rl/utils/constraints.py
+++
@@ -1,5 +0,0 @@
-from air_hockey_challenge.constraints import ConstraintList
-
-class ConstraintCollection(ConstraintList):
-    def output_dim(self):
-        return sum(constr.output_dim for constr in self.constraints.values())
